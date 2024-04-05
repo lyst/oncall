b3afa345b48560604d171c36716a019a274d79c7
Tristan Keen <tristan.keen@lyst.com>
2024-04-05 16:29:33 +0100

    Quieten version diff

--- engine/settings/base.py
+++ engine/settings/base.py
@@ -9,7 +9,7 @@ from firebase_admin import credentials, initialize_app
 
 from common.utils import getenv_boolean, getenv_integer, getenv_list
 
-VERSION = "dev-oss"
+VERSION = "1.3.115"
 SEND_ANONYMOUS_USAGE_STATS = getenv_boolean("SEND_ANONYMOUS_USAGE_STATS", default=True)
 
 # License is OpenSource or Cloud
